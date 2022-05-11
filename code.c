5ede9bcc1ec94cc8a0ccc457de143d85921d8690
IUlok <elfbrus@gmail.com>
2022-05-11 15:34:14 +0300

    cringe

--- code.c
+++ code.c
@@ -1,48 +1,58 @@
 #include <stdio.h>
-#include <string.h>
+#include <math.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct Node
-{
-    struct Node *next;
-    int value;
-} Spisok;
+typedef struct author {
+  struct author *next;
+  char name[20];
+  char spec[30];
+  char year[5];
+  char pages[10];
+} t_list;
+// typedef struct author Author;
+void show(t_list c);
+// struct author inpstud(void);
+
+t_list *create_node(char *name, char *spec, char *year, char *pages) { /*Создание элемента*/
+  t_list *node = (t_list *)malloc(sizeof(t_list));
+  strcpy(node -> name, name);
+  strcpy(node -> spec,spec);
+  strcpy(node -> year, year);
+  strcpy(node -> pages,pages);
+  node -> next = NULL;  /*Следующий указатель будет нулевым*/
+  return node;
+}
+
+// void push_front(t_list **list, char *name, char *spec, char *year, char *pages) { /*Создание следующего элемента*/
+//   t_list *new_element = create_node(name, spec, year, pages);
+//   new_element -> next = *list;  /*Присваивания предыдущего указателя указателем на этот элемент*/
+//   *list = new_element;
+// }
 
 int main() {
-  Spisok *create(int data)
-  {
-  // Выделение памяти под корень списка
-  Spisok *tmp = (Spisok*)malloc(sizeof(Spisok));
-  // Присваивание значения узлу
-  tmp -> value = data;
-  // Присваивание указателю на следующий элемент значения NULL
-  tmp -> next = NULL;
-  return(tmp);
+  FILE *authors = fopen("authors.txt", "r");
+  FILE *vybor = fopen ("vybor.txt", "w");
+  char napr[100];
+  char name, spec, year, pages;
+  t_list x;
+  struct author *p = &x;
+
+  printf("%s", "Write napravlenye:\nMath\nPhysics\nProgramming\n");
+  scanf("%s", napr);
+  printf("Your enter: %s. Results copy to vybor.txt\n", napr);
+  t_list *list = create_node("TEST", "TEST", 0, 0);
+  while (!feof(authors)) {
+    fscanf(authors, "%s %s %s %s", name, spec, year, pages);
+    // push_front(&list, &name, &spec, &year, &pages);
+    if (!strcmp(napr, p->spec)) {
+      printf("%s %s %s %s\n", list -> name, list -> spec, list -> year, list -> pages);
+      fprintf(vybor, "%s %s %s %s\n", list -> name, list -> spec, list -> year, list -> pages);
+      // list = list -> next;
+    }
   }
-  // FILE *authors = fopen("authors.txt", "r");
-  // FILE *vybor = fopen ("vybor.txt", "w");
-  // char napr[100];
-  // Author x;
-  // struct author *p = &x;
-  //
-  // printf("%s", "Set needed napravlenye:\nMath\nPhysics\nProgramming\n");
-  // scanf("%s", &napr);
-  // printf("Your set: %s. Res of ur zapros enter to vybor.txt\n", napr);
-  // while (!feof(authors)) {
-  //   fscanf(authors, "%s", p->name);
-  //   fscanf(authors, "%s", p->spec);
-  //   fscanf(authors, "%s", p->year);
-  //   fscanf(authors, "%s\n", p->pages);
-  //   if (!strcmp(napr, p->spec)) {
-  //     fprintf(vybor, "Name: %s", p->name);
-  //     fprintf(vybor, "\tNapravlenye: %s", p->spec);
-  //     fprintf(vybor, "\tYear: %s", p->year);
-  //     fprintf(vybor, "\tPages: %s", p->pages);
-  //     fprintf(vybor, "\n");
-  //   }
-  // }
-  //
-  // fclose(authors);
-  // fclose(vybor);
+
+  fclose(authors);
+  fclose(vybor);
   return 0;
 }
